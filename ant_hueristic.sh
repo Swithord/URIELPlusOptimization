2be79f56140e4d7d31f45a2e9b0f88eb651c8be5
LoMein123 <jbnouse@gmail.com>
2025-07-05 14:35:59 -0400

    SLURM commands

--- ant_hueristic.sh
+++ ant_hueristic.sh
@@ -5,7 +5,17 @@
 #SBATCH --account=def-annielee
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=junbin.cheng@ontariotechu.net
-#SBATCH --output-outfile/myfile-%j.out
+#SBATCH --output=outfile/myfile-%j.out
+
+set -e
 
 # Load the necessary modules
 module load python/3.10
+module load scipy-stack
+
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+
+pip install --no-index -r requirements.txt
+python ant_hueristic.py
